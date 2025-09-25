17547c7e69214cef364c43e7771bf525b6ee4778
Marina Crespo <marina.crespo@valtech.com>
2025-09-25 16:11:41 -0300

    initial design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,69 @@
-import { Box, Button } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
-    <Box p={4}>
-      <Button bg="#C06226" color="white" size="xl">Hello Marina</Button>
-    </Box >
+    <Box
+      w="100vw"
+      h="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      bg="pink.100"
+      p={2}
+    >
+      <Box
+        w="80%"
+        h="66.6%"
+        bg="pink.300"
+        mb={4}
+        display="flex"
+        p={2}
+      >
+        <Box
+          w="40%"
+          h="100%"
+          mr={2}
+          bg="blue.200"
+        >
+          Backlog
+        </Box>
+        <Box
+          w="60%"
+          h="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            h="30%"
+            bg="blue.300"
+            mb={2}
+          >
+            Current
+          </Box>
+          <Box
+            h="70%"
+            bg="pink.100"
+          >
+            Comandos
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        w="80%"
+        bg="pink.300"
+        h="33.3%"
+        p={2}
+      >
+        <Box
+          w="100%"
+          h="100%"
+          bg="blue.200"
+        >
+          [   ___   ]
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/components/Backlog/index.jsx
@@ -0,0 +1,7 @@
+export const Backlog = () => {
+    return (
+        <div width="50%" height="70%">
+            Backlog
+        </div>
+    )
+}
